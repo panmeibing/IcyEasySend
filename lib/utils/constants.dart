aa019cb2b8b864fffd0405ddc071951c5313e58d
mickypan123 <mickypan@grantit.io>
2026-02-07 15:38:24 +0800

    Updated the default server port

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -7,13 +7,13 @@ class AppConstants {
   /// This is the default port used by the HTTP server to listen for incoming
   /// file transfer requests. The server will try this port first, and if it's
   /// occupied, it will try subsequent ports up to [maxServerPort].
-  static const int defaultPort = 8080;
+  static const int defaultPort = 9527;
 
   /// Maximum port number to try when starting the server
   ///
   /// If [defaultPort] is occupied, the server will try ports in the range
   /// [defaultPort] to [maxServerPort] until it finds an available one.
-  static const int maxServerPort = 8090;
+  static const int maxServerPort = 9537;
 
   // Timeout constants
 
